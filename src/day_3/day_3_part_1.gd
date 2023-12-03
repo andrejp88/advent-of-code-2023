64cbb662e111f7580058fe37beb01480f17a269f
Andrej Petrović <1616076-andrej88@users.noreply.gitlab.com>
2023-12-03 18:01:18 +0000

    Day 3, Part 1

--- src/day_3/day_3_part_1.gd
+++ src/day_3/day_3_part_1.gd
@@ -17,7 +17,8 @@ func repopulate_tilemap(path: String) -> void:
 		var line := lines[line_idx]
 		for character_idx in range(line.length()):
 			var character := line[character_idx]
-			tilemap.set_cell(0, Vector2(character_idx, line_idx), 1, get_atlas_coords(character))
+			if character != ".":
+				tilemap.set_cell(1, Vector2(character_idx, line_idx), 1, get_atlas_coords(character))
 
 
 func get_atlas_coords(character: String) -> Vector2:
@@ -42,10 +43,120 @@ func get_atlas_coords(character: String) -> Vector2:
 		"$": return Vector2(1, 2)
 		"%": return Vector2(2, 2)
 		"&": return Vector2(3, 2)
-		".": return Vector2(6, 7)
 		_:
 			printerr("Unexpected character: '", character, "'")
 			return Vector2(7, 7)
+
+
+func get_digit(tile_atlas_coords: Vector2i) -> int:
+	if tile_atlas_coords.y == 0:
+		return tile_atlas_coords.x
+	elif tile_atlas_coords.y == 1 and tile_atlas_coords.x <= 1:
+		return 8 + tile_atlas_coords.x
+	else:
+		return -100000
+
+
+func highlight_all_numbers() -> void:
+	for tile_coords: Vector2i in tilemap.get_used_cells(1):
+		var tile_atlas_coords := tilemap.get_cell_atlas_coords(1, tile_coords)
+		if is_number_cell(tile_atlas_coords):
+			tilemap.set_cell(0, tile_coords, 1, Vector2(5, 7))
+
+
+func highlight_part_numbers() -> void:
+	for tile_coords: Vector2i in tilemap.get_used_cells(1):
+		var tile_atlas_coords := tilemap.get_cell_atlas_coords(1, tile_coords)
+		if is_symbol_cell(tile_atlas_coords):
+			var already_visited := {tile_coords: true}
+			var neighbours := get_surrounding_cells_with_corners(tile_coords)
+			for neighbour: Vector2i in neighbours:
+				if not (neighbour in already_visited):
+					flood_fill(neighbour, Vector2(4, 7), already_visited)
+
+
+func get_part_numbers() -> Array[int]:
+	var part_numbers: Array[int] = []
+
+	for tile_coords: Vector2i in tilemap.get_used_cells(0):
+		var tile_atlas_coords_layer_0 := tilemap.get_cell_atlas_coords(0, tile_coords)
+		var tile_atlas_coords_layer_1 := tilemap.get_cell_atlas_coords(1, tile_coords)
+
+		if is_number_cell(tile_atlas_coords_layer_1) and tile_atlas_coords_layer_0 == Vector2i(4, 7):
+
+			# If it's the left-most number cell, parse the number, else we have already parsed it probably
+			var left_cell_coords := tile_coords - Vector2i(1, 0)
+			var left_cell_atlas_coords := tilemap.get_cell_atlas_coords(1, left_cell_coords)
+			var left_cell_data := tilemap.get_cell_tile_data(1, left_cell_coords)
+
+			#if left_cell_data == null or not is_number_cell(left_cell_coords):
+			if is_number_cell(left_cell_atlas_coords):
+				continue
+
+			var this_number := get_digit(tile_atlas_coords_layer_1)
+			var i := 1
+
+			while true:
+				var next_tile_coords := tile_coords + Vector2i(i, 0)
+				var next_tile_atlas_coords := tilemap.get_cell_atlas_coords(1, next_tile_coords)
+
+				if not is_number_cell(next_tile_atlas_coords):
+					break
+
+				this_number *= 10
+				this_number += get_digit(next_tile_atlas_coords)
+
+				i += 1
+
+			part_numbers.push_back(this_number)
+
+	return part_numbers
+
+
+func flood_fill(tile_coords: Vector2i, highlight_tile_atlas_coords: Vector2i, already_visited: Dictionary) -> void:
+
+	var tile_atlas_coords := tilemap.get_cell_atlas_coords(1, tile_coords)
+	already_visited[tile_coords] = true
+
+	if is_number_cell(tile_atlas_coords):
+		tilemap.set_cell(0, tile_coords, 1, highlight_tile_atlas_coords)
+
+		for neighbour: Vector2i in get_horizontal_neighbours(tile_coords):
+			if not (neighbour in already_visited):
+				flood_fill(neighbour, highlight_tile_atlas_coords, already_visited)
+
+
+func get_surrounding_cells_with_corners(tile_coords: Vector2i) -> Array[Vector2i]:
+	return  [
+		tilemap.get_neighbor_cell(tile_coords, TileSet.CELL_NEIGHBOR_TOP_LEFT_CORNER),
+		tilemap.get_neighbor_cell(tile_coords, TileSet.CELL_NEIGHBOR_TOP_SIDE),
+		tilemap.get_neighbor_cell(tile_coords, TileSet.CELL_NEIGHBOR_TOP_RIGHT_CORNER),
+		tilemap.get_neighbor_cell(tile_coords, TileSet.CELL_NEIGHBOR_LEFT_SIDE),
+		tilemap.get_neighbor_cell(tile_coords, TileSet.CELL_NEIGHBOR_RIGHT_SIDE),
+		tilemap.get_neighbor_cell(tile_coords, TileSet.CELL_NEIGHBOR_BOTTOM_LEFT_CORNER),
+		tilemap.get_neighbor_cell(tile_coords, TileSet.CELL_NEIGHBOR_BOTTOM_SIDE),
+		tilemap.get_neighbor_cell(tile_coords, TileSet.CELL_NEIGHBOR_BOTTOM_RIGHT_CORNER),
+	]
+
+func get_horizontal_neighbours(tile_coords: Vector2i) -> Array[Vector2i]:
+	return  [
+		tilemap.get_neighbor_cell(tile_coords, TileSet.CELL_NEIGHBOR_LEFT_SIDE),
+		tilemap.get_neighbor_cell(tile_coords, TileSet.CELL_NEIGHBOR_RIGHT_SIDE),
+	]
+
+
+func is_number_cell(tile_atlas_coords: Vector2i) -> bool:
+	return (
+		tile_atlas_coords.y == 0 or
+		(tile_atlas_coords.y == 1 and tile_atlas_coords.x <= 1)
+	)
+
+
+func is_symbol_cell(tile_atlas_coords: Vector2i) -> bool:
+	return (
+		(tile_atlas_coords.y == 1 and tile_atlas_coords.x >= 2) or
+		(tile_atlas_coords.y == 2 and tile_atlas_coords.x <= 3)
+	)
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -83,3 +194,14 @@ func _on_use_test_input_pressed() -> void:
 
 func _on_use_real_input_pressed() -> void:
 	repopulate_tilemap("res://src/day_3/input.txt")
+
+
+func _on_highlight_part_numbers_button_pressed() -> void:
+	highlight_all_numbers()
+	highlight_part_numbers()
+
+	var part_numbers := get_part_numbers()
+	$CanvasLayer/VBoxContainer/HBoxContainer2/NumPartNumbers.text = "Part Number Count: %d" % part_numbers.size()
+
+	var part_number_sum := part_numbers.reduce(func(acc: int, e: int) -> int: return acc + e) as int
+	$CanvasLayer/VBoxContainer/HBoxContainer2/PartNumberSum.text = "Part Number Sum: %d" % part_number_sum
