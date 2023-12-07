40d1355c9f260806aed47724b7b64ad4b050a573
Andrej Petrović <1616076-andrej88@users.noreply.gitlab.com>
2023-12-07 20:45:48 +0000

    Day 7, Part 2

--- src/day_7/day_7.gd
+++ src/day_7/day_7.gd
@@ -11,7 +11,7 @@ enum HandType {
 	five_of_a_kind,
 }
 
-const rank_to_index := {
+const rank_to_index_part_1 := {
 	"2": 0,
 	"3": 1,
 	"4": 2,
@@ -27,18 +27,41 @@ const rank_to_index := {
 	"A": 12,
 }
 
+const rank_to_index_part_2 := {
+	"J": 0,
+	"2": 1,
+	"3": 2,
+	"4": 3,
+	"5": 4,
+	"6": 5,
+	"7": 6,
+	"8": 7,
+	"9": 8,
+	"T": 9,
+	"Q": 10,
+	"K": 11,
+	"A": 12,
+}
+
 
 func part_1(input: String) -> int:
-
 	var lines := Array(input.split("\n", false))
+	return calculate_winnings(lines, get_hand_type_part_1, rank_to_index_part_1)
 
+
+func part_2(input: String) -> int:
+	var lines := Array(input.split("\n", false))
+	return calculate_winnings(lines, get_hand_type_part_2, rank_to_index_part_2)
+
+
+func calculate_winnings(lines: Array, get_hand_type_fn: Callable, rank_to_index_mapping: Dictionary) -> int:
 	lines.sort_custom(
 		func(a: String, b: String) -> bool:
 			var a_hand := a.get_slice(" ", 0)
 			var b_hand := b.get_slice(" ", 0)
 
-			var a_hand_type := get_hand_type(a_hand)
-			var b_hand_type := get_hand_type(b_hand)
+			var a_hand_type: HandType = get_hand_type_fn.call(a_hand)
+			var b_hand_type: HandType = get_hand_type_fn.call(b_hand)
 
 			if a_hand_type != b_hand_type:
 				return a_hand_type < b_hand_type
@@ -48,7 +71,7 @@ func part_1(input: String) -> int:
 				var b_card := b_hand[i]
 
 				if a_card != b_card:
-					return rank_to_index[a_card] < rank_to_index[b_card]
+					return rank_to_index_mapping[a_card] < rank_to_index_mapping[b_card]
 
 			# At this point they are equal and we can do whatever I guess
 			assert(a_hand == b_hand, "Expected both hands to be equal but they're still different: a = %s, b = %s" % [a_hand, b_hand])
@@ -67,17 +90,13 @@ func part_1(input: String) -> int:
 	return total_winnings
 
 
-func part_2(input: String) -> int:
-	return 0
-
-
-func get_hand_type(hand: String) -> HandType:
+func get_hand_type_part_1(hand: String) -> HandType:
 	var counts: Array[int] = []
 	counts.resize(13)
 	counts.fill(0)
 
 	for card: String in hand:
-		counts[rank_to_index[card]] += 1
+		counts[rank_to_index_part_1[card]] += 1
 
 	var hand_descriptor := counts.filter(func(count: int) -> bool: return count != 0)
 	hand_descriptor.sort()
@@ -98,4 +117,58 @@ func get_hand_type(hand: String) -> HandType:
 
 		_:
 			printerr("Unexpected hand_descriptor: ", hand_descriptor)
+			return HandType.high_card
+
+
+func get_hand_type_part_2(hand: String) -> HandType:
+	var counts: Array[int] = []
+	counts.resize(13)
+	counts.fill(0)
+
+	var num_jokers := 0
+
+	for card: String in hand:
+		if card == "J":
+			num_jokers += 1
+		else:
+			counts[rank_to_index_part_2[card]] += 1
+
+	var hand_descriptor := counts.filter(func(count: int) -> bool: return count != 0)
+	hand_descriptor.sort()
+	hand_descriptor.reverse()
+
+	var hand_descriptor_sum: int = hand_descriptor.reduce(func(acc: int, e: int) -> int: return acc + e, 0)
+
+	assert(
+		hand_descriptor_sum + num_jokers == 5,
+		"hand_descriptor_sum + num_jokers was expected to equal five, but hand_descriptor is %s and there are %s jokers." % [hand_descriptor, num_jokers]
+	)
+
+	match [num_jokers, hand_descriptor]:
+		[0, [5]]: return HandType.five_of_a_kind
+		[0, [4, 1]]: return HandType.four_of_a_kind
+		[0, [3, 2]]: return HandType.full_house
+		[0, [3, 1, 1]]: return HandType.three_of_a_kind
+		[0, [2, 2, 1]]: return HandType.two_pair
+		[0, [2, 1, 1, 1]]: return HandType.one_pair
+		[0, [1, 1, 1, 1, 1]]: return HandType.high_card
+
+		[1, [4]]: return HandType.five_of_a_kind
+		[1, [3, 1]]: return HandType.four_of_a_kind
+		[1, [2, 2]]: return HandType.full_house
+		[1, [2, 1, 1]]: return HandType.three_of_a_kind
+		[1, [1, 1, 1, 1]]: return HandType.one_pair
+
+		[2, [3]]: return HandType.five_of_a_kind
+		[2, [2, 1]]: return HandType.four_of_a_kind
+		[2, [1, 1, 1]]: return HandType.three_of_a_kind
+
+		[3, [2]]: return HandType.five_of_a_kind
+		[3, [1, 1]]: return HandType.four_of_a_kind
+
+		[4, [1]]: return HandType.five_of_a_kind
+		[5, []]: return HandType.five_of_a_kind
+
+		_:
+			printerr("Unexpected hand_descriptor/joker combination: ", hand_descriptor, " with ", num_jokers, " jokers")
 			return HandType.high_card
--- src/day_7/day_7_test.gd
+++ src/day_7/day_7_test.gd
@@ -30,7 +30,7 @@ func test_part_2_example() -> void:
 	var input := fa.get_as_text()
 	fa.close()
 
-	assert_eq(day.part_2(input), 123_456_789)
+	assert_eq(day.part_2(input), 5905)
 
 
 func test_part_2_real() -> void:
@@ -42,11 +42,54 @@ func test_part_2_real() -> void:
 	pass_test("passed")
 
 
-func test_get_hand_type() -> void:
-	assert_eq(day.get_hand_type("AAAAA"), Day7.HandType.five_of_a_kind)
-	assert_eq(day.get_hand_type("AA8AA"), Day7.HandType.four_of_a_kind)
-	assert_eq(day.get_hand_type("23332"), Day7.HandType.full_house)
-	assert_eq(day.get_hand_type("TTT98"), Day7.HandType.three_of_a_kind)
-	assert_eq(day.get_hand_type("23432"), Day7.HandType.two_pair)
-	assert_eq(day.get_hand_type("A23A4"), Day7.HandType.one_pair)
-	assert_eq(day.get_hand_type("23456"), Day7.HandType.high_card)
+func test_get_hand_type_part_1() -> void:
+	assert_eq(day.get_hand_type_part_1("AAAAA"), Day7.HandType.five_of_a_kind)
+	assert_eq(day.get_hand_type_part_1("AA8AA"), Day7.HandType.four_of_a_kind)
+	assert_eq(day.get_hand_type_part_1("23332"), Day7.HandType.full_house)
+	assert_eq(day.get_hand_type_part_1("TTT98"), Day7.HandType.three_of_a_kind)
+	assert_eq(day.get_hand_type_part_1("23432"), Day7.HandType.two_pair)
+	assert_eq(day.get_hand_type_part_1("A23A4"), Day7.HandType.one_pair)
+	assert_eq(day.get_hand_type_part_1("23456"), Day7.HandType.high_card)
+
+
+func test_get_hand_type_part_2() -> void:
+	# The previous examples should work the same
+	assert_eq(day.get_hand_type_part_2("AAAAA"), Day7.HandType.five_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("AA8AA"), Day7.HandType.four_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("23332"), Day7.HandType.full_house)
+	assert_eq(day.get_hand_type_part_2("TTT98"), Day7.HandType.three_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("23432"), Day7.HandType.two_pair)
+	assert_eq(day.get_hand_type_part_2("A23A4"), Day7.HandType.one_pair)
+	assert_eq(day.get_hand_type_part_2("23456"), Day7.HandType.high_card)
+
+	# But J is now a Joker, and should be treated as whatever makes for the most favorable hand
+	# These four examples are from the problem description:
+	assert_eq(day.get_hand_type_part_2("QJJQ2"), Day7.HandType.four_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("T55J5"), Day7.HandType.four_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("KTJJT"), Day7.HandType.four_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("QQQJA"), Day7.HandType.four_of_a_kind)
+
+	# And these are my own:
+	# Non-joker cards are all different
+	assert_eq(day.get_hand_type_part_2("2345J"), Day7.HandType.one_pair)
+	assert_eq(day.get_hand_type_part_2("234JJ"), Day7.HandType.three_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("23JJJ"), Day7.HandType.four_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("2JJJJ"), Day7.HandType.five_of_a_kind)
+
+	# Non-joker cards contain a pair already
+	assert_eq(day.get_hand_type_part_2("2344J"), Day7.HandType.three_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("233JJ"), Day7.HandType.four_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("22JJJ"), Day7.HandType.five_of_a_kind)
+
+	# Non-joker cards contain a two-pair already
+	assert_eq(day.get_hand_type_part_2("2233J"), Day7.HandType.full_house)
+
+	# Non-joker cards contain a three-of-a-kind already
+	assert_eq(day.get_hand_type_part_2("2333J"), Day7.HandType.four_of_a_kind)
+	assert_eq(day.get_hand_type_part_2("222JJ"), Day7.HandType.five_of_a_kind)
+
+	# Non-joker cards contain a four-of-a-kind already
+	assert_eq(day.get_hand_type_part_2("2222J"), Day7.HandType.five_of_a_kind)
+
+	# Oops, all jokers!
+	assert_eq(day.get_hand_type_part_2("JJJJJ"), Day7.HandType.five_of_a_kind)
