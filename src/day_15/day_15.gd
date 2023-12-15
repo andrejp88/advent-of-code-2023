3e27e447e435e26e31c85712de8b1b0629e7fa98
Andrej Petrović <1616076-andrej88@users.noreply.gitlab.com>
2023-12-15 14:02:46 +0000

    Day 15, Part 2

--- src/day_15/day_15.gd
+++ src/day_15/day_15.gd
@@ -3,12 +3,54 @@ class_name Day15
 const Util := preload("res://src/util.gd")
 
 
+class Box:
+	var lenses: Array[Dictionary]
+
+	func put_lens(label: String, focal_length: int) -> void:
+		for lens: Dictionary in lenses:
+			if lens["label"] == label:
+				lens["focal_length"] = focal_length
+				return
+
+		lenses.push_back({ "label": label, "focal_length": focal_length })
+
+	func remove_lens(label: String) -> void:
+		var to_remove_idx := -1
+
+		for lens_idx: int in lenses.size():
+			if lenses[lens_idx]["label"] == label:
+				to_remove_idx = lens_idx
+				break
+
+		if to_remove_idx != -1:
+			lenses.remove_at(to_remove_idx)
+
+
+
 func part_1(input: String) -> int:
 	return Array(input.strip_edges().split(",")).map(holiday_hash).reduce(Util.sumi)
 
 
 func part_2(input: String) -> int:
-	return 0
+	var instructions: Array[String] = []
+	instructions.assign(input.strip_edges().split(","))
+
+	var boxes: Array[Box] = []
+	boxes.resize(256)
+
+	for idx: int in boxes.size():
+		boxes[idx] = Day15.Box.new()
+
+	for instruction: String in instructions:
+		operate_on_boxes(instruction, boxes)
+
+	var total_focusing_power := 0
+
+	for box_idx: int in boxes.size():
+		for lens_idx: int in boxes[box_idx].lenses.size():
+			total_focusing_power += (box_idx + 1) * (lens_idx + 1) * boxes[box_idx].lenses[lens_idx]["focal_length"]
+
+	return total_focusing_power
 
 
 func holiday_hash(input: String) -> int:
@@ -20,3 +62,17 @@ func holiday_hash(input: String) -> int:
 		result %= 256
 
 	return result
+
+
+func operate_on_boxes(instruction: String, boxes: Array[Box]) -> void:
+	var regex := RegEx.new()
+	regex.compile(r"([a-z]+)(-|=([1-9]))")
+	var regex_result := regex.search(instruction)
+	var label := regex_result.get_string(1)
+	var box_idx := holiday_hash(label)
+
+	if regex_result.get_string(2) == "-":
+		boxes[box_idx].remove_lens(label)
+	else:
+		var focal_length := int(regex_result.get_string(3))
+		boxes[box_idx].put_lens(label, focal_length)
--- src/day_15/day_15_test.gd
+++ src/day_15/day_15_test.gd
@@ -30,7 +30,7 @@ func test_part_2_example() -> void:
 	var input := fa.get_as_text()
 	fa.close()
 
-	assert_eq(day.part_2(input), 123_456_789)
+	assert_eq(day.part_2(input), 145)
 
 
 func test_part_2_real() -> void:
@@ -55,3 +55,53 @@ func test_holiday_hash() -> void:
 	assert_eq(day.holiday_hash("pc-"), 48)
 	assert_eq(day.holiday_hash("pc=6"), 214)
 	assert_eq(day.holiday_hash("ot=7"), 231)
+
+
+func test_operate_on_lenses() -> void:
+	var boxes: Array[Day15.Box] = []
+	boxes.resize(256)
+
+	for idx: int in boxes.size():
+		boxes[idx] = Day15.Box.new()
+
+	day.operate_on_boxes("rn=1", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}])
+
+	day.operate_on_boxes("cm-", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}])
+
+	day.operate_on_boxes("qp=3", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}])
+	assert_eq(boxes[1].lenses, [{"label": "qp", "focal_length": 3}])
+
+	day.operate_on_boxes("cm=2", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}, {"label": "cm", "focal_length": 2}])
+	assert_eq(boxes[1].lenses, [{"label": "qp", "focal_length": 3}])
+
+	day.operate_on_boxes("qp-", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}, {"label": "cm", "focal_length": 2}])
+	assert_eq(boxes[1].lenses, [])
+
+	day.operate_on_boxes("pc=4", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}, {"label": "cm", "focal_length": 2}])
+	assert_eq(boxes[3].lenses, [{"label": "pc", "focal_length": 4}])
+
+	day.operate_on_boxes("ot=9", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}, {"label": "cm", "focal_length": 2}])
+	assert_eq(boxes[3].lenses, [{"label": "pc", "focal_length": 4}, {"label": "ot", "focal_length": 9}])
+
+	day.operate_on_boxes("ab=5", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}, {"label": "cm", "focal_length": 2}])
+	assert_eq(boxes[3].lenses, [{"label": "pc", "focal_length": 4}, {"label": "ot", "focal_length": 9}, {"label": "ab", "focal_length": 5}])
+
+	day.operate_on_boxes("pc-", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}, {"label": "cm", "focal_length": 2}])
+	assert_eq(boxes[3].lenses, [{"label": "ot", "focal_length": 9}, {"label": "ab", "focal_length": 5}])
+
+	day.operate_on_boxes("pc=6", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}, {"label": "cm", "focal_length": 2}])
+	assert_eq(boxes[3].lenses, [{"label": "ot", "focal_length": 9}, {"label": "ab", "focal_length": 5}, {"label": "pc", "focal_length": 6}])
+
+	day.operate_on_boxes("ot=7", boxes)
+	assert_eq(boxes[0].lenses, [{"label": "rn", "focal_length": 1}, {"label": "cm", "focal_length": 2}])
+	assert_eq(boxes[3].lenses, [{"label": "ot", "focal_length": 7}, {"label": "ab", "focal_length": 5}, {"label": "pc", "focal_length": 6}])
