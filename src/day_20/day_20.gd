e27b841a3500084e26e88fefdac907ca1ac8cd06
Andrej Petrović <1616076-andrej88@users.noreply.gitlab.com>
2023-12-22 14:49:22 +0000

    Day 20, Part 1

---
+++ src/day_20/day_20.gd
@@ -0,0 +1,180 @@
+class_name Day20
+
+const Util := preload("res://src/util.gd")
+
+
+const OFF := false
+const ON := true
+const LOW := 3
+const HIGH := 7
+
+
+func part_1(input: String) -> int:
+
+	var network := parse_input(input)
+	var low_pulse_count := 0
+	var high_pulse_count := 0
+
+	for i in 1000:
+		var pulses_to_send: Array[Dictionary] = [{ "type": LOW, "from": "button", "to": "broadcaster" }]
+
+		while not pulses_to_send.is_empty():
+			var current_pulse: Dictionary = pulses_to_send.pop_front()
+
+			if current_pulse["type"] == LOW:
+				low_pulse_count += 1
+			else:
+				high_pulse_count += 1
+
+			if current_pulse["to"] not in network.keys():
+				continue
+
+
+			var next_pulses := send_pulse(network, current_pulse["type"], current_pulse["from"], current_pulse["to"])
+			pulses_to_send.append_array(next_pulses)
+
+	return low_pulse_count * high_pulse_count
+
+
+func part_2(input: String) -> int:
+	return 0
+
+
+func parse_input(input: String) -> Dictionary:
+
+	var result := {
+		"button": {
+			"type": "button",
+			"out": ["broadcaster"],
+		},
+	}
+
+	var lines := input.split("\n", false)
+
+	for line: String in lines:
+		var line_regex := RegEx.new()
+		line_regex.compile(r"(broadcaster|[%&]\w+) -> ((\w+, )*\w+)")
+		var line_match := line_regex.search(line)
+		var type_and_name_string := line_match.get_string(1)
+		var outputs_string := line_match.get_string(2)
+		var outputs_list := Array(outputs_string.split(", ", false))
+
+		if type_and_name_string == "broadcaster":
+			result["broadcaster"] = {
+				"type": "broadcaster",
+				"in": ["button"],
+				"out": outputs_list,
+			}
+			continue
+
+		var type := type_and_name_string[0]
+		var name := type_and_name_string.substr(1)
+
+		result[name] = {
+			"type": type,
+			"in": [],
+			"out": outputs_list,
+		}
+
+		match type:
+			"%":
+				result[name]["state"] = OFF
+
+			"&":
+				result[name]["state"] = {}
+
+			_:
+				printerr("Unexpected module type: ", type)
+
+
+	for module_name: String in result.keys():
+		if module_name in ["button", "broadcaster"]:
+			continue
+
+		for potential_input_module_name: String in result.keys():
+			if potential_input_module_name == module_name:
+				continue
+
+			if module_name in result[potential_input_module_name]["out"]:
+				result[module_name]["in"].append(potential_input_module_name)
+
+				if result[module_name]["type"] == "&":
+					result[module_name]["state"][potential_input_module_name] = LOW
+
+	return result
+
+
+func send_pulse(network: Dictionary, pulse_type: int, from: String, to: String) -> Array[Dictionary]:
+	var next_pulses: Array[Dictionary] = []
+
+	var subject: Dictionary = network[to]
+
+	match subject["type"]:
+		"broadcaster":
+			next_pulses.assign(subject["out"].map(
+				func(dest: String) -> Dictionary:
+					return {
+						"type": pulse_type,
+						"from": to,
+						"to": dest,
+					}
+			))
+
+		"%":
+			if pulse_type == LOW:
+				if subject["state"] == OFF:
+					# For some reason, setting the key directly doesn't work
+					subject.merge({ "state": ON }, true)
+
+					next_pulses.assign(subject["out"].map(
+						func(dest: String) -> Dictionary:
+							return {
+								"type": HIGH,
+								"from": to,
+								"to": dest,
+							}
+					))
+
+				else:
+					# For some reason, setting the key directly doesn't work
+					subject.merge({ "state": OFF }, true)
+
+					next_pulses.assign(subject["out"].map(
+						func(dest: String) -> Dictionary:
+							return {
+								"type": LOW,
+								"from": to,
+								"to": dest,
+							}
+					))
+
+		"&":
+			subject["state"].merge({ from: pulse_type }, true)
+			next_pulses.assign(subject["out"].map(
+				func(dest: String) -> Dictionary:
+					return {
+						"type": (
+							LOW
+							if subject["state"].values().all(func(e: int) -> bool: return e == HIGH) else
+							HIGH
+						),
+						"from": to,
+						"to": dest,
+					}
+			))
+
+		_:
+			printerr("Unhandled type: ", subject["type"])
+
+	return next_pulses
+
+
+func stringify_network_state(network: Dictionary) -> String:
+	var pieces: Array[String] = []
+
+	for module_name: String in network.keys():
+		var module: Dictionary = network[module_name]
+		if module["type"] == "%":
+			pieces.append("%s:%s" % [module_name, 1 if module["state"] else 0])
+
+	return ",".join(pieces)
---
+++ src/day_20/day_20_test.gd
@@ -0,0 +1,251 @@
+extends GutTest
+
+
+var day: Day20
+
+
+const parsed_test_input_1 := {
+	"button": {
+		"type": "button",
+		"out": ["broadcaster"],
+	},
+	"broadcaster": {
+		"type": "broadcaster",
+		"in": ["button"],
+		"out": ["a", "b", "c"],
+	},
+	"a": {
+		"type": "%",
+		"in": ["broadcaster", "inv"],
+		"out": ["b"],
+		"state": Day20.OFF,
+	},
+	"b": {
+		"type": "%",
+		"in": ["broadcaster", "a"],
+		"out": ["c"],
+		"state": Day20.OFF,
+	},
+	"c": {
+		"type": "%",
+		"in": ["broadcaster", "b"],
+		"out": ["inv"],
+		"state": Day20.OFF,
+	},
+	"inv": {
+		"type": "&",
+		"in": ["c"],
+		"out": ["a"],
+		"state": { "c": Day20.LOW, }
+	},
+}
+
+const parsed_test_input_2 := {
+	"button": {
+		"type": "button",
+		"out": ["broadcaster"],
+	},
+	"broadcaster": {
+		"type": "broadcaster",
+		"in": ["button"],
+		"out": ["a"],
+	},
+	"a": {
+		"type": "%",
+		"in": ["broadcaster"],
+		"out": ["inv", "con"],
+		"state": Day20.OFF,
+	},
+	"inv": {
+		"type": "&",
+		"in": ["a"],
+		"out": ["b"],
+		"state": { "a": Day20.LOW }
+	},
+	"b": {
+		"type": "%",
+		"in": ["inv"],
+		"out": ["con"],
+		"state": Day20.OFF,
+	},
+	"con": {
+		"type": "&",
+		"in": ["a", "b"],
+		"out": ["output"],
+		"state": { "a": Day20.LOW, "b": Day20.LOW },
+	},
+}
+
+
+func before_each() -> void:
+	day = preload("res://src/day_20/day_20.gd").new()
+
+
+func test_part_1_example_1() -> void:
+	var fa := FileAccess.open("res://src/day_20/test_input_1.txt", FileAccess.READ)
+	var input := fa.get_as_text()
+	fa.close()
+
+	assert_eq(day.part_1(input), 32_000_000)
+
+
+func test_part_1_example_2() -> void:
+	var fa := FileAccess.open("res://src/day_20/test_input_2.txt", FileAccess.READ)
+	var input := fa.get_as_text()
+	fa.close()
+
+	assert_eq(day.part_1(input), 11_687_500)
+
+
+func test_part_1_real() -> void:
+	var fa := FileAccess.open("res://src/day_20/input.txt", FileAccess.READ)
+	var input := fa.get_as_text()
+	fa.close()
+
+	print("Day 20, Part 1: ", day.part_1(input))
+	pass_test("passed")
+
+
+func test_part_2_example_2() -> void:
+	var fa := FileAccess.open("res://src/day_20/test_input_2.txt", FileAccess.READ)
+	var input := fa.get_as_text()
+	fa.close()
+
+	assert_eq(day.part_2(input), 123_456_789)
+
+
+func test_part_2_real() -> void:
+	var fa := FileAccess.open("res://src/day_20/input.txt", FileAccess.READ)
+	var input := fa.get_as_text()
+	fa.close()
+
+	print("Day 20, Part 2: ", day.part_2(input))
+	pass_test("passed")
+
+
+func test_parse_input_1() -> void:
+	var fa := FileAccess.open("res://src/day_20/test_input_1.txt", FileAccess.READ)
+	var input := fa.get_as_text()
+	fa.close()
+
+	var result := day.parse_input(input)
+
+	assert_eq(result, parsed_test_input_1)
+
+
+func test_parse_input_2() -> void:
+	var fa := FileAccess.open("res://src/day_20/test_input_2.txt", FileAccess.READ)
+	var input := fa.get_as_text()
+	fa.close()
+
+	var result := day.parse_input(input)
+	assert_eq(result, parsed_test_input_2)
+
+
+func test_send_pulse_broadcaster() -> void:
+	var network_copy := parsed_test_input_1.duplicate(true)
+	var network_before_call := network_copy.duplicate(true)
+	assert_eq(day.send_pulse(network_copy, Day20.LOW, "button", "broadcaster"), [
+		{
+			"type": Day20.LOW,
+			"from": "broadcaster",
+			"to": "a",
+		},
+		{
+			"type": Day20.LOW,
+			"from": "broadcaster",
+			"to": "b",
+		},
+		{
+			"type": Day20.LOW,
+			"from": "broadcaster",
+			"to": "c",
+		},
+	])
+
+	assert_eq(parsed_test_input_1, network_before_call)
+
+
+func test_send_pulse_flip_flop_low_when_off() -> void:
+	var network := parsed_test_input_1.duplicate(true)
+	var network_before_call := network.duplicate(true)
+	var expected_network_after_call := network.duplicate(true)
+	expected_network_after_call["a"]["state"] = Day20.ON
+
+	assert_eq(day.send_pulse(network, Day20.LOW, "broadcaster", "a"), [
+		{
+			"type": Day20.HIGH,
+			"from": "a",
+			"to": "b",
+		},
+	])
+
+
+	assert_ne(network, network_before_call)
+	assert_eq(network, expected_network_after_call)
+
+
+func test_send_pulse_flip_flop_low_when_on() -> void:
+	var network := parsed_test_input_1.duplicate(true)
+	network["a"]["state"] = Day20.ON
+	var network_before_call := network.duplicate(true)
+	var expected_network_after_call := network.duplicate(true)
+	expected_network_after_call["a"]["state"] = Day20.OFF
+
+	assert_eq(day.send_pulse(network, Day20.LOW, "broadcaster", "a"), [
+		{
+			"type": Day20.LOW,
+			"from": "a",
+			"to": "b",
+		},
+	])
+
+
+	assert_ne(network, network_before_call)
+	assert_eq(network, expected_network_after_call)
+
+
+func test_send_pulse_flip_flop_high() -> void:
+	var network := parsed_test_input_1.duplicate(true)
+	var network_before_call := network.duplicate(true)
+
+	assert_eq(day.send_pulse(network, Day20.HIGH, "broadcaster", "a"), [])
+	assert_eq(network, network_before_call)
+
+
+func test_send_pulse_conjunction_low() -> void:
+	var network := parsed_test_input_2.duplicate(true)
+	var network_before_call := network.duplicate(true)
+	var expected_network_after_call := network.duplicate(true)
+
+	assert_eq(day.send_pulse(network, Day20.LOW, "a", "con"), [
+		{
+			"type": Day20.HIGH,
+			"from": "con",
+			"to": "output",
+		}
+	])
+
+	assert_eq(network, network_before_call)
+
+
+func test_send_pulse_conjunction_high() -> void:
+	var network := parsed_test_input_2.duplicate(true)
+	var network_before_call := network.duplicate(true)
+	var expected_network_after_call := network.duplicate(true)
+	expected_network_after_call["con"]["state"]["a"] = Day20.HIGH
+
+	assert_eq(day.send_pulse(network, Day20.HIGH, "a", "con"), [
+		{
+			"type": Day20.HIGH,
+			"from": "con",
+			"to": "output",
+		}
+	])
+
+	assert_eq(network, expected_network_after_call)
+
+
+func test_stringify_network_state() -> void:
+	assert_eq(day.stringify_network_state(parsed_test_input_1), "a:0,b:0,c:0")
+	assert_eq(day.stringify_network_state(parsed_test_input_2), "a:0,b:0")
